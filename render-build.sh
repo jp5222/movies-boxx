0936e8dfd7ad3767165680d352428a68c897f025
Jay Prakash <jay5222347@gmail.com>
2025-07-08 16:25:13 +0530

    fix: ensure gunicorn installs

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 echo "Installing dependencies..."
+pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "Downloading similarity.pkl..."
 curl -L -o similarity.pkl "https://drive.google.com/uc?export=download&id=1eaRn28XCyrjwzAhWfB_C8yNHtRSNK3Mi"
+
+echo "Installed packages:"
+pip list
+
