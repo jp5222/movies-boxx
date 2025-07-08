9f5adc8a15b862189a74b3b0e926b9731ed4927a
Jay Prakash <jay5222347@gmail.com>
2025-07-08 14:59:11 +0530

    Add runtime.txt and fix deployment issues

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo "Setting up Python environment..."
-pip install --upgrade pip setuptools wheel
-
-echo "Installing dependencies..."
-pip install -r requirements.txt
-
 echo "Downloading similarity.pkl..."
 curl -L -o similarity.pkl "https://drive.google.com/uc?export=download&id=1eaRn28XCyrjwzAhWfB_C8yNHtRSNK3Mi"
+
