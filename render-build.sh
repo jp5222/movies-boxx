f8cd9e0654e237d85c2c818adabfc0cf6b6530a7
Jay Prakash <jay5222347@gmail.com>
2025-07-08 17:15:17 +0530

    Fix build: add runtime.txt and gunicorn

--- render-build.sh
+++ render-build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
+curl -o similarity.pkl https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/similarity.pkl
 
-echo "Installing dependencies..."
-pip install --upgrade pip
-pip install -r requirements.txt
-
-echo "Downloading similarity.pkl..."
-curl -L -o similarity.pkl "https://drive.google.com/uc?export=download&id=1eaRn28XCyrjwzAhWfB_C8yNHtRSNK3Mi"
 
