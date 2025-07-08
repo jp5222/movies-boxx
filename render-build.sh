292bbd9c53a299af8e56510a83ccd94e4ed627b2
Jay Prakash <jay5222347@gmail.com>
2025-07-08 14:18:30 +0530

    Fix Python version and build script

--- render-build.sh
+++ render-build.sh
@@ -3,5 +3,6 @@
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
-echo "Downloading similarity.pkl for the app..."
-python app.py --download-only
+echo "Downloading similarity.pkl..."
+curl -L -o similarity.pkl "https://drive.google.com/uc?export=download&id=1eaRn28XCyrjwzAhWfB_C8yNHtRSNK3Mi"
+
