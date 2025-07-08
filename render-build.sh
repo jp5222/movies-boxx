bc9c3f2e81d01bfbf5c93a4b9f05c9458dca1c20
Jay Prakash <jay5222347@gmail.com>
2025-07-08 16:47:12 +0530

    Add build script and fix deployment setup

--- render-build.sh
+++ render-build.sh
@@ -7,6 +7,3 @@ pip install -r requirements.txt
 echo "Downloading similarity.pkl..."
 curl -L -o similarity.pkl "https://drive.google.com/uc?export=download&id=1eaRn28XCyrjwzAhWfB_C8yNHtRSNK3Mi"
 
-echo "Installed packages:"
-pip list
-
