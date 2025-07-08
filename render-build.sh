fa47fb00e1c34615bbbd8fe73ce6f9630ae877d4
Jay Prakash <jay5222347@gmail.com>
2025-07-08 15:06:21 +0530

    Fix: install dependencies and download similarity.pkl

--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
 echo "Downloading similarity.pkl..."
 curl -L -o similarity.pkl "https://drive.google.com/uc?export=download&id=1eaRn28XCyrjwzAhWfB_C8yNHtRSNK3Mi"
-
